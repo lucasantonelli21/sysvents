4c42fadf300a79cd820ed33e59e1b8a55a4b66fb
Lucas Antonelli <lucaslba2105@gmail.com>
2025-03-06 17:09:11 -0300

    blank

---
+++ resources/views/components/blank.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SysVents</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendor.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/styles.min.css') }}" />
+    <script src="{{asset('assets/js/vendor.min.js')}}"></script>
+    <script type="module" src="{{asset('assets/js/scripts.min.js')}}"></script>
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
