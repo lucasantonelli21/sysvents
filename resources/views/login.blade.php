1903e0cfd9b91405366e50a9109091d1afbd246f
Lucas Antonelli <lucaslba2105@gmail.com>
2025-03-06 17:26:18 -0300

    tela-login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,24 @@
 <x-blank>
 
     <div class="card">
-        <div class="card-header"></div>
-        <div class="card-body">oi</div>
-        <div class="card-footer"></div>
+        <div class="card-header">
+            <h2 class="text-center">Por Favor Realize Login!</h2>
+        </div>
+        <form action="">
+            <div class="card-body">
+                <div class="form-group">
+                    <label class="form-label">E-mail</label>
+                    <input class="form-control" type="email" name="email" required value="{{ old('email') }}"/>
+                </div>
+            <div class="form-group">
+                    <label class="form-label">Senha</label>
+                    <input class="form-control" type="password" name="password" required value="{{ old('password') }}"/>
+                </div>
+            </div>
+            <div class="card-footer text-end">
+                <button class="btn btn-success" type="submit">Logar</button>
+            </div>
+        </form>
     </div>
 
 </x-blank>
