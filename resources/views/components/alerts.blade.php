008f401448d4aed375522a1f0df5e7158f6be4e3
Lucas Antonelli <lucaslba2105@gmail.com>
2025-03-07 16:35:17 -0300

    alert-change

--- resources/views/components/alerts.blade.php
+++ resources/views/components/alerts.blade.php
@@ -7,20 +7,27 @@
 
 @if ($messagesError)
 
-    <div class="alert alert-danger" role="alert">
 
-        @foreach ($messagesError as $message)
-            {{ $message[0] }} <br>
-        @endforeach
-
+    <div class="alert alert-danger d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+        <div>
+            @foreach ($messagesError as $message)
+                {{ $message[0] }} <br>
+            @endforeach
+        </div>
     </div>
 
 @endif
 
 @if ($messagesSuccess)
-
-    <div class="alert alert-success" role="alert">
-        {{ $messagesSuccess }}
+    <div class="alert alert-success d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+        </svg>
+        <div>
+            {{ $messagesSuccess }}
+        </div>
     </div>
-
 @endif
