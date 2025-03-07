2dd4ef37fc7943b17348498e2141511d81ec4cf9
caueobm <caue.obm@gmail.com>
2025-03-07 16:57:27 -0300

    factories e seeders finalizado

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Nette\Utils\Random;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -29,8 +30,26 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'cpf' => fake()->numberBetween(10000000000, 99999999999),
+            'phone' => fake()->phoneNumber(),
+            'birth_date' => fake()->date(),
+            'is_admin' => random_int(1, 10) == 1 ? 'true' : 'false',
+
+
         ];
     }
+
+    // $table->id();
+    // $table->string('name');
+    // $table->string('email')->unique();
+    // $table->timestamp('email_verified_at')->nullable();
+    // $table->string('password');
+    // $table->string('cpf');
+    // $table->string('phone');
+    // $table->date('birth_date');
+    // $table->boolean('is_admin');
+    // $table->rememberToken();
+    // $table->timestamps();
 
     /**
      * Indicate that the model's email address should be unverified.
