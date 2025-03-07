56fafe233f7612e04df03cd6968d167fae193344
KezerSouza <kezer@discente.ufg.br>
2025-03-07 17:40:15 -0300

    Comentários desnecessários removidos de web.php

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 
 
-// Auth
-// Route::get('/', [AuthController::class, 'showLogin'])->name('login');
-// Route::get('/registrar', [AuthController::class, 'showRegister']);
-// Route::post('/registrar', [AuthController::class, 'store']);
-// Route::post('/', [AuthController::class, 'authenticate']);
-// Route::post('/logout', [AuthController::class, 'logout']);
-
 Route::get('/login', function(){
     return view('login');
 });
