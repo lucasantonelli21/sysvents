624434935cd5a20abf670c7059cd8dd6a08ea3b2
KezerSouza <kezer@discente.ufg.br>
2025-03-10 18:02:47 -0300

    Merge com a tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,94 @@
 
     <div class="home pt-5">
 
-             <div class="text-center">
+            <div class="text-center">
 
-                 <div>
-                     <h1 class="title">Bem-vindo à Sysvents</h1>
+                <div>
+                    <h1 class="title">Bem-vindo à Sysvents</h1>
+                </div>
+
+                <div class="mb-5">
+                    <h2 class="subtitle">Conectando você aos melhores eventos -
+                        <span>Descubra</span>,
+                        <span>viva</span>,
+                        <span>aproveite</span>
+                    </h2>
+                </div>
+
+                 <div class="">
+                    <img src="{{ asset('images/evento-image.webp') }}" class="event-image rounded">
                  </div>
 
-                 <div class="mb-5">
-                     <h2 class="subtitle">Conectando você aos melhores eventos -
-                         <span>Descubra</span>,
-                         <span>viva</span>,
-                         <span>aproveite</span>
-                     </h2>
-                 </div>
 
-                 <div class="text-center">
-                     <img src="{{ asset('images/evento-image-black.webp') }}" class="event-image rounded">
-                 </div>
-             </div>
+            </div>
+
+
+            <div class="mt-5">
+                <h2>Sobre nós</h2>
+                <h4>A Sysvents é uma empresa focada em conectar pessoas a experiências únicas por meio de uma plataforma eficiente de divulgação e compra de ingressos para eventos.</h4>
+            </div>
+
+        </div>
+
+
+        <div class="events-container mt-5">
+            <div class="text-center">
+                <h2>Eventos</h2>
+            </div>
+
+            <div class="media-scroller d-flex gap-3">
+                <div class="media-element rounded">
+                    <img src="{{ asset('images/eventfake1.jpeg') }}" class="rounded">
+                    <div class="image-overlay d-flex justify-content-center flex-column">
+                        <h2 class="event-title text-center">Evento 1</h2>
+                        <p class="mx-2 event-description">Um evento.</p>
+                    </div>
+                </div>
+
+                <div class="media-element rounded">
+                    <img src="{{ asset('images/eventfake2.jpeg') }}" class="rounded">
+                    <div class="image-overlay d-flex justify-content-center flex-column">
+                        <h2 class="event-title text-center">Evento 2</h2>
+                        <p class="mx-2 event-description">Um evento.</p>
+                    </div>
+                </div>
+
+                <div class="media-element rounded">
+                    <img src="{{ asset('images/eventfake3.jpeg') }}" class="rounded">
+                    <div class="image-overlay d-flex justify-content-center flex-column">
+                        <h2 class="event-title text-center">Evento 3</h2>
+                        <p class="mx-2 event-description">Um evento.</p>
+                    </div>
+                </div>
+
+                <div class="media-element rounded">
+                    <img src="{{ asset('images/eventfake4.jpeg') }}" class="rounded">
+                    <div class="image-overlay d-flex justify-content-center flex-column">
+                        <h2 class="event-title text-center">Evento 4</h2>
+                        <p class="mx-2 event-description">Um evento.</p>
+                    </div>
+                </div>
+
+                <div class="media-element rounded">
+                    <img src="{{ asset('images/eventfake5.jpeg') }}" class="rounded">
+                    <div class="image-overlay d-flex justify-content-center flex-column">
+                        <h2 class="event-title text-center">Evento 5</h2>
+                        <p class="mx-2 event-description">Um evento.</p>
+                    </div>
+                </div>
+
+                <div class="media-element rounded">
+                    <img src="{{ asset('images/eventfake6.jpeg') }}" class="rounded">
+                    <div class="image-overlay d-flex justify-content-center flex-column">
+                        <h2 class="event-title text-center">Evento 6</h2>
+                        <p class="mx-2 event-description">Um evento.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
 
     </div>
 
