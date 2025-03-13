6f7389011638c9362134de2fbc2bddee28545ce0
KezerSouza <kezer@discente.ufg.br>
2025-03-13 14:43:38 -0300

    Cor azul nas seções da view event, textos abaixo da imagem reorganizados.

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -5,24 +5,26 @@
         <div class="d-flex m-5">
 
 
-            <div class="left-section bg-dark p-3">
+            <div class="left-section p-3">
 
                 <div>
                     <div class="event-image-container">
                         <img src="{{ asset('images/eventfake1.jpeg') }}" class="" alt="">
                     </div>
 
-                    <p>Evento: {{ $event->name }}</p>
-                    <p>Tema: {{ $event->theme }}</p>
-                    <p>Data: {{ $event->start_date == $event->end_date ? \Carbon\Carbon::parse($event->start_date)->format('d/m/Y')  : \Carbon\Carbon::parse($event->start_date)->format('d/m/Y').' - '.\Carbon\Carbon::parse($event->end_date)->format('d/m/Y') }}</p>
-                    <p>Botão comprar ingresso</p>
+                    <div class="mt-2">
+                        <p class="mb-2">Evento: {{ $event->name }}</p>
+                        <p class="mb-2">Tema: {{ $event->theme }}</p>
+                        <p class="mb-2">Data: {{ $event->start_date == $event->end_date ? \Carbon\Carbon::parse($event->start_date)->format('d/m/Y')  : \Carbon\Carbon::parse($event->start_date)->format('d/m/Y').' - '.\Carbon\Carbon::parse($event->end_date)->format('d/m/Y') }}</p>
+                        <p class="mb-2">Botão comprar ingresso</p>
+                    </div>
                 </div>
 
 
             </div>
 
 
-            <div class="right-section bg-dark d-flex flex-column">
+            <div class="right-section d-flex flex-column">
                 <div class="m-3 d-flex flex-column flex-grow-1">
                     <h2 class="text-center event-title m-0 pb-3">{{ $event->name }}</h2>
                     <p>{{ $event->description }}</p>
