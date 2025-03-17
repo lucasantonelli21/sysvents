3c76fa214fd5fb610b59f06e34fec67317cc3ab2
KezerSouza <kezer@discente.ufg.br>
2025-03-17 12:29:10 -0300

    Evento quando clicado levar para sua própria página com todas informações daquele evento

--- resources/views/events/library.blade.php
+++ resources/views/events/library.blade.php
@@ -76,13 +76,13 @@
 
                 <div class="d-flex justify-content-between flex-wrap gap-3">
                     @foreach ($events as $event)
-                        <div class="media-element mb-1 rounded" href="{{ url('eventos/'.$event->id) }}">
+                        <a class="media-element mb-1 rounded" href="{{ url('eventos/'.$event->id) }}">
                             <img src="{{$event->image_path ? asset($event->image_path) : asset('images/default-event-image.jpeg')}}" class="rounded">
                             <div class="image-overlay d-flex justify-content-center flex-column rounded">
-                                <h2 class="event-title text-center">{{ $event->name }}</h2>
+                                <h3 class="event-title text-center">{{ $event->name }}</h3>
                                 <p class="mx-2 event-description">{{ $event->description }}</p>
                             </div>
-                        </div>
+                        </a>
 
                     @endforeach
 
