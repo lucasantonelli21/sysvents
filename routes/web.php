3e05b06f64dcea957eb1e9d8defcfe1ad8b0da60
Lucas Antonelli <lucaslba2105@gmail.com>
2025-03-18 12:47:10 -0300

    ticket-batch-routes

--- routes/web.php
+++ routes/web.php
@@ -108,20 +108,8 @@ Route::prefix('/painel')->name('panel.')->middleware(VerifyLogin::class, Authent
         Route::delete('/{id}/deletar',  [ExhibitorController::class, 'delete'])->name('delete');
     });
 
-<<<<<<< Updated upstream
     Route::prefix('/transações')->name('transactions.')->group(function() {
         Route::get('/',                 [TransactionController::class,'index'])->name('index');
-=======
-    Route::prefix('/transações')->name('transactions.')->group(function () {
-        Route::get('/',                 [TransactionController::class, 'index'])->name('index');
-        Route::get('/cadastro',         [TransactionController::class, 'showRegister'])->name('register');
-
-        Route::get('/criar',            [TransactionController::class, 'createOrEdit'])->name('create');
-        Route::get('/{id}/editar',      [TransactionController::class, 'createOrEdit'])->name('edit');
-
-        Route::post('/salvar',          [TransactionController::class, 'save'])->name('save');
-        Route::put('/salvar',           [TransactionController::class, 'save'])->name('update');
->>>>>>> Stashed changes
         Route::delete('/{id}/deletar',  [TransactionController::class, 'delete'])->name('delete');
     });
 });
