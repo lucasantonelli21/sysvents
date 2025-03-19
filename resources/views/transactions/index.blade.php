0bff63d5715e9fc9bd933431883e984733d725a3
Lucas Antonelli <lucaslba2105@gmail.com>
2025-03-19 12:36:32 -0300

    bota-voltar

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -8,6 +8,13 @@
             <div class="card-row">
                 <div class="select-pagination">
                     <form class="form-pagination" action="{{ route('panel.transactions.index') }}">
+                        <a href="{{ url()->previous() }}" class="btn-back btn btn-outline-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="icon-large bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+                            </svg>
+                        </a>
                         <select class="paginator-selector" name="pagination">
                             @foreach ($paginations as $value)
                                 <option {{ $value == request()->pagination ? 'selected' : '' }}
@@ -82,57 +89,59 @@
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="card-body ">
-            <div class="table-responsive ">
-                <table class="table table-striped custom-table">
+    <div class="card-body ">
+        <div class="table-responsive ">
+            <table class="table table-striped custom-table">
 
+                <tr>
+                    <th>ID da compra</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Valor Total</th>
+                    <th>Tickets</th>
+                    <th>Deletar</th>
+                </tr>
+                @foreach ($transactions as $transaction)
                     <tr>
-                        <th>ID da compra</th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Valor Total</th>
-                        <th>Tickets</th>
-                        <th>Deletar</th>
-                    </tr>
-                    @foreach ($transactions as $transaction)
-                        <tr>
-                            <td>{{ $transaction->id }}</td>
-                            <td>{{ $transaction->user->name }}</td>
-                            <td>{{ $transaction->user->email }}</td>
-                            <td>R${{ $transaction->amount }}</td>
-                            <td>{{ $transaction->total_tickets }}</td>
-                            <td>
-                                <div class= "table-buttons">
-                                    <a href="{{ route('panel.transactions.show',[$transaction->id]) }}" class="btn btn-outline-primary">
+                        <td>{{ $transaction->id }}</td>
+                        <td>{{ $transaction->user->name }}</td>
+                        <td>{{ $transaction->user->email }}</td>
+                        <td>R${{ $transaction->amount }}</td>
+                        <td>{{ $transaction->total_tickets }}</td>
+                        <td>
+                            <div class= "table-buttons">
+                                <a href="{{ route('panel.transactions.show', [$transaction->id]) }}"
+                                    class="btn btn-outline-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                        <path
+                                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                                    </svg>
+                                </a>
+                                <form action="{{ route('panel.transactions.delete', [$transaction->id]) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-outline-danger">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                            fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                             <path
-                                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                            <path
+                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                                         </svg>
-                                    </a>
-                                    <form action="{{ route('panel.transactions.delete', [$transaction->id]) }}"
-                                        method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-outline-danger">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                                                <path
-                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
 
-                </table>
-            </div>
+            </table>
         </div>
+    </div>
     </div>
     <div class="card-footer text-end">
         {{ $transactions->links() }}
