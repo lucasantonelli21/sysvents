25795770fcc78b1327bcac43166119f00a61622b
Lucas Antonelli <lucaslba2105@gmail.com>
2025-03-20 14:01:29 -0300

    bugfix:form-users

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -13,12 +13,10 @@
     <div class="page-form-users card ">
         <div class="card-header">
             <div class="row-title-button">
-                <a href="{{ url()->previous() }}" class="btn-back btn btn-outline-light">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="icon-large bi bi-arrow-left-circle" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-                      </svg>
+                <a href="{{ url()->previous() }}" class="icons btn-back btn btn-outline-light">
+                    <img src="{{ asset('images/icons/arrow-left-circle.svg') }}" alt="">
                 </a>
-                <h2 class="title text-center">{{ $user->id ? 'Atualize o Usuário ' . $user->name : 'Se Registre Aqui' }}</h2>
+                <h2 class="title text-center">{{ $user->id ? 'Atualize o Usuário ' . $user->name : (Auth::check() ? 'Cadastre Um Novo Usuário' : 'Se Cadastre Aqui') }}</h2>
             </div>
         </div>
         <form action="{{ route($user->id ? 'users.update' : 'users.create', $user->id ?? [$user->id]) }}" method="POST">
